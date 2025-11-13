172b6e626fae6a895170aabc32ac2dfc61bd9145
DAVAD92GZ <ds.ruizc12@uniandes.edu.co>
2025-11-12 23:24:16 -0500

    Delete Entrega unica/Proyecto1Dpoo/src/module-info.java

--- Entrega unica/Proyecto1Dpoo/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module Proyecto1Dpoo {
-    requires com.fasterxml.jackson.databind;
-    requires com.fasterxml.jackson.core;
-    requires com.fasterxml.jackson.annotation;
-    requires com.fasterxml.jackson.datatype.jsr310;
-
-}
-
